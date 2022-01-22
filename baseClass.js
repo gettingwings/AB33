07ec952eb909b068891be19a1aa2b74df9955c7b
gettingwings <74932161+gettingwings@users.noreply.github.com>
2022-01-22 10:26:44 +0530

    Update baseClass.js

--- baseClass.js
+++ baseClass.js
@@ -8,7 +8,7 @@ class BaseClass{
         this.body = Bodies.rectangle(x,y,width, height, options);
         this.width = width;
         this.height = height;
-        this.image = loadImage("/sprites/base.png");  // placeholder
+        this.image = loadImage("./sprites/base.png");  // placeholder
 
         World.add(world, this.body);
 
