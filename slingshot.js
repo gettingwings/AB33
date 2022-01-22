021504db14a508c2cb766327bcee2852620ec699
gettingwings <74932161+gettingwings@users.noreply.github.com>
2022-01-22 10:26:26 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -11,9 +11,9 @@ class SlingShot{
           this.sling = Constraint.create(options);
           World.add(world, this.sling);
 
-          this.sling1 = loadImage("sprites/sling1.png");
-          this.sling2 = loadImage("sprites/sling2.png");
-          this.sling3 = loadImage("sprites/sling3.png");
+          this.sling1 = loadImage("./sprites/sling1.png");
+          this.sling2 = loadImage("./sprites/sling2.png");
+          this.sling3 = loadImage("./sprites/sling3.png");
     }
 
     fly(){
